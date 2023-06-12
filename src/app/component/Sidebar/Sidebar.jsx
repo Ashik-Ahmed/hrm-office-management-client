3a98c10b96e1cfab46bc946cbe908afd5de36f9b
unknown <ashikahmed121@gmail.com>
2023-06-12 22:59:57 +0600

    sidebar added

--- src/app/component/Sidebar/Sidebar.jsx
+++ src/app/component/Sidebar/Sidebar.jsx
@@ -11,7 +11,62 @@ import './customcss.css'
 const Sidebar = () => {
     return (
         <div className='sticky top-0 min-w-[300px] h-screen bg-white text-gray-700'>
+            <div className='flex mt-6'>
+                <div className='flex flex-col px-4 justify-between'>
+                    <ul className='flex flex-col gap-6 mt-2'>
+                        {/* <div className='bg-gray-600 hover:bg-gray-500 duration-200 cursor-pointer text-white p-3 -ml-1.5 flex rounded-full'>
+                            <i className='pi pi-th-large' style={{ fontSize: '.75rem' }}></i>
+                        </div> */}
+                        <Image src={logo} alt='logo' width='40' height='40' />
+                        <i className='mt-4 pi pi-search cursor-pointer hover:text-sky-400 duration-200' style={{ fontSize: '1.3rem' }}></i>
+                        <i className='pi pi-calendar cursor-pointer hover:text-sky-400 duration-200' style={{ fontSize: '1.3rem' }}></i>
+                        <i className='pi pi-id-card cursor-pointer hover:text-sky-400 duration-200' style={{ fontSize: '1.3rem' }}></i>
+                        <i className='pi pi-comments cursor-pointer hover:text-sky-400 duration-200' style={{ fontSize: '1.3rem' }}></i>
+                        <i className='pi pi-folder cursor-pointer hover:text-sky-400 duration-200' style={{ fontSize: '1.3rem' }}></i>
+                    </ul>
+                    <ul className='flex flex-col gap-6'>
+                        {/* <i className='pi pi-comments hover:text-sky-400 duration-200' style={{ fontSize: '1.4rem' }}></i> */}
+                        {/* <i className='pi pi-folder hover:text-sky-400 duration-200' style={{ fontSize: '1.4rem' }}></i> */}
+                        <div>
+                            <span class="relative flex h-3 w-3 float-right -ml-1.5 -mt-1">
+                                <span class="custom-ping absolute inline-flex h-full w-full rounded-full bg-pink-400 opacity-75 -ml-0.5 -mt-0.5"></span>
+                                <span class="relative inline-flex rounded-full h-2 w-2 bg-pink-500"></span>
+                            </span>
+                            <HiMail size={20} color='gray' />
+                        </div>
+                        <div>
+                            <span class="relative flex h-3 w-3 float-right -ml-1.5 -mt-1">
+                                <span class="custom-ping animation-duration-600 absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75 -ml-0.5 -mt-0.5"></span>
+                                <span class="relative inline-flex rounded-full h-2 w-2 bg-gray-500"></span>
+                            </span>
+                            <MdNotifications size={20} color='gray' />
+                        </div>
+                        <RiSettings5Fill size={20} color='gray' className='cursor-pointer custom-spin' />
+                        <FaUser size={20} color='gray' className='cursor-pointer' />
+                    </ul>
+                </div>
 
+                <div className='w-full'>
+                    <h2 className='text-2xl hover:bg-red-500'>Infozillion</h2>
+
+                    <div className='mt-8 text-sm'>
+                        <p className='text-xs hover:bg-red-500' style={{ fontFamily: 'revert' }}>DIRECTORIES</p>
+                        <ul className=' mt-4 text-gray-600'>
+                            <Link href='/' style={{ fontFamily: 'revert' }} className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-hourglass'></i> HRMS</Link>
+                            <Link href='/' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Dashboard</Link>
+                            <Link href='/users' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Users</Link>
+                            <Link href='/department' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Department</Link>
+                            <Link href='/employee' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Employee</Link>
+                            <Link href='/activities' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Activities</Link>
+                            <Link href='/holidays' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Holidays</Link>
+                            <Link href='/events' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Events</Link>
+                            <Link href='/payroll' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Payroll</Link>
+                            <Link href='/accounts' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Accounts</Link>
+                            <Link href='/' className='py-2 hover:tracking-wider hover:border-r-2 hover:border-r-violet-500  duration-200 cursor-pointer flex gap-x-4'> <i className='pi pi-ellipsis-h scale-75'></i> Report</Link>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
