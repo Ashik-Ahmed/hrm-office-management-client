a8d8a5c8bc4a0cc4a1c2df02b5d9125b21479f16
unknown <ashikahmed121@gmail.com>
2023-06-21 23:24:56 +0600

    leave data table added

--- src/app/leave/page.jsx
+++ src/app/leave/page.jsx
@@ -1,9 +1,22 @@
+'use client'
+
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
 import React from 'react';
 
 const Leave = () => {
     return (
         <div>
-            leave page
+            <h2>Leave History</h2>
+
+            <div>
+                <DataTable tableStyle={{ minWidth: '50rem' }}>
+                    <Column field="code" header="Code"></Column>
+                    <Column field="name" header="Name"></Column>
+                    <Column field="category" header="Category"></Column>
+                    <Column field="quantity" header="Quantity"></Column>
+                </DataTable>
+            </div>
         </div>
     );
 };
