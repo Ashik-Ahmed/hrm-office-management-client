5949f4a8db7cd633271c37ea7519525681f812bf
unknown <ashikahmed121@gmail.com>
2023-06-22 23:18:36 +0600

    dummy leave data added to show in the table

--- src/app/leave/page.jsx
+++ src/app/leave/page.jsx
@@ -5,16 +5,24 @@ import { DataTable } from 'primereact/datatable';
 import React from 'react';
 
 const Leave = () => {
+
+    const leaves = [
+        { id: "1", leave: "Casual", taken: 3, available: 5 },
+        { id: "2", leave: "Annual", taken: 3, available: 5 },
+        { id: "3", leave: "Sick", taken: 3, available: 5 },
+        { id: "4", leave: "Emergency", taken: 3, available: 5 },
+        { id: "5", leave: "Maternity", taken: 3, available: 5 },
+    ]
+
     return (
         <div>
             <h2>Leave History</h2>
 
-            <div>
-                <DataTable tableStyle={{ minWidth: '50rem' }}>
-                    <Column field="code" header="Code"></Column>
-                    <Column field="name" header="Name"></Column>
-                    <Column field="category" header="Category"></Column>
-                    <Column field="quantity" header="Quantity"></Column>
+            <div className='w-1/2'>
+                <DataTable value={leaves} size='small' tableStyle={{ minWidth: '50rem' }}>
+                    <Column field="leave" header="Leave"></Column>
+                    <Column field="taken" header="Taken"></Column>
+                    <Column field="available" header="Available"></Column>
                 </DataTable>
             </div>
         </div>
