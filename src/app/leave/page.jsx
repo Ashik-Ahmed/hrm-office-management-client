27987e6b60c9e74eb070b23eaf6854778339ce75
unknown <ashikahmed121@gmail.com>
2023-07-20 15:26:44 +0600

    leave application form in dialog created

--- src/app/leave/page.jsx
+++ src/app/leave/page.jsx
@@ -1,10 +1,21 @@
 'use client'
 
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
-import React from 'react';
+import { Dialog } from 'primereact/dialog';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import React, { useState } from 'react';
 
 const Leave = () => {
+
+    const [leaveFormDialog, setLeaveFormDialog] = useState(false)
+    const [selectedLeave, setSelectedLeave] = useState(null);
+    const [fromDate, setFromDate] = useState(null)
+    const [toDate, setToDate] = useState(null)
+    const [rejoinDate, setRejoinDate] = useState(null)
 
     const leaves = [
         { id: "1", leave: "Casual", total: 10, taken: 3, balance: 5 },
@@ -56,12 +67,47 @@ const Leave = () => {
             </div>
 
             <div className='mt-4 shadow-xl'>
-                <DataTable value={applicationHistory} header={leaveApplicationTableHeader} size='small'>
-                    <Column field="leaveType" header="Leave Type"></Column>
-                    <Column field="applied" header="Application Date"></Column>
-                    <Column field="total" header="Total"></Column>
-                    <Column field="applicationStatus" header="Status"></Column>
-                </DataTable>
+                <Button onClick={() => setLeaveFormDialog(true)} label='Apply for Leave' className='p-button-sm' />
+                <Dialog header="Header" visible={leaveFormDialog} onHide={() => setLeaveFormDialog(false)}
+                    style={{ width: '50vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
+                    <div>
+                        <form className='flex flex-col gap-2'>
+                            <Dropdown value={selectedLeave} onChange={(e) => setSelectedLeave(e.value)} options={leaves} optionLabel="leave" placeholder="Select Leave Type" className='w-full' />
+                            <div className="flex gap-x-4">
+                                <span className="p-float-label min-w-1/3">
+                                    <Calendar inputId="from_date" value={fromDate} onChange={(e) => setFromDate(e.value)} />
+                                    <label htmlFor="from_date">From</label>
+                                </span>
+                                <span className="p-float-label min-w-1/3">
+                                    <Calendar inputId="to_date" value={toDate} onChange={(e) => setToDate(e.value)} />
+                                    <label htmlFor="to_date">To</label>
+                                </span>
+                                <span className="p-float-label min-w-1/3">
+                                    <InputText id="total_day" />
+                                    <label htmlFor="total_day">Total day</label>
+                                </span>
+                            </div>
+                            <span className="p-float-label">
+                                <Calendar inputId="to_date" value={rejoinDate} onChange={(e) => setRejoinDate(e.value)} className='w-full' />
+                                <label htmlFor="to_date">Re-joining date</label>
+                            </span>
+                            <span className="p-float-label min-w-1/3">
+                                <InputText id="purpose" className='w-full' />
+                                <label htmlFor="purpose">Purpose of Leave</label>
+                            </span>
+
+                            <Button label='Submit' />
+                        </form>
+                    </div>
+                </Dialog>
+                <div className='mt-1'>
+                    <DataTable value={applicationHistory} header={leaveApplicationTableHeader} size='small'>
+                        <Column field="leaveType" header="Leave Type"></Column>
+                        <Column field="applied" header="Application Date"></Column>
+                        <Column field="total" header="Total"></Column>
+                        <Column field="applicationStatus" header="Status"></Column>
+                    </DataTable>
+                </div>
             </div>
         </div>
     );
