84f550e5fe5fa8f3c19bb21a86b0379751b3306e
unknown <ashikahmed121@gmail.com>
2023-07-20 11:12:07 +0600

    table added for leave application history

--- src/app/leave/page.jsx
+++ src/app/leave/page.jsx
@@ -14,12 +14,18 @@ const Leave = () => {
         { id: "5", leave: "Maternity", total: 10, taken: 3, balance: 5 },
     ]
 
-    const leaveHistory = [
-        { id: "1", applied: "21-04-2023", total: "3" },
-        { id: "2", applied: "21-04-2023", total: "3" },
-        { id: "3", applied: "21-04-2023", total: "3" },
-        { id: "4", applied: "21-04-2023", total: "3" },
-        { id: "5", applied: "21-04-2023", total: "3" }
+    const applicationHistory = [
+        { id: "1", leaveType: "Casual", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "2", leaveType: "Annual", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "3", leaveType: "Sick", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "4", leaveType: "Emergency", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "5", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "6", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "7", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "8", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "9", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "10", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
+        { id: "11", leaveType: "Maternity", applied: "21-04-2023", total: "3", applicationStatus: 'HR Approved' },
     ]
 
     const leaveHistoryTableHeader = () => {
@@ -30,14 +36,31 @@ const Leave = () => {
         )
     }
 
+    const leaveApplicationTableHeader = () => {
+        return (
+            <div>
+                <p>Leave Application History</p>
+            </div>
+        )
+    }
+
     return (
-        <div>
+        <div className='py-2'>
             <div className='w-1/2 shadow-xl'>
-                <DataTable value={leaves} header={leaveHistoryTableHeader} size='small' >
+                <DataTable value={leaves} header={leaveHistoryTableHeader} size='small'>
                     <Column field="leave" header="Leave Type"></Column>
                     <Column field="total" header="Total"></Column>
                     <Column field="taken" header="Taken"></Column>
                     <Column field="balance" header="Balance"></Column>
+                </DataTable>
+            </div>
+
+            <div className='mt-4 shadow-xl'>
+                <DataTable value={applicationHistory} header={leaveApplicationTableHeader} size='small'>
+                    <Column field="leaveType" header="Leave Type"></Column>
+                    <Column field="applied" header="Application Date"></Column>
+                    <Column field="total" header="Total"></Column>
+                    <Column field="applicationStatus" header="Status"></Column>
                 </DataTable>
             </div>
         </div>
