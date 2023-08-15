50aceb7b43ffa29f732a792d8ac33f89707c8670
unknown <ashikahmed121@gmail.com>
2023-08-15 20:01:16 +0600

    all dialog created and open close functionality added on manage leave page

--- src/app/component/PendingLeaveTable/PendingLeaveTable.jsx
+++ src/app/component/PendingLeaveTable/PendingLeaveTable.jsx
@@ -8,9 +8,15 @@ import { AiOutlineCheckCircle } from 'react-icons/ai';
 import { MdOutlineCancel, MdRemoveRedEye } from 'react-icons/md';
 
 const PendingLeave = ({ pendingLeaveApplications }) => {
-    console.log(pendingLeaveApplications);
 
-    const [leaveDetails, setLeaveDetails] = useState(null);
+    const [detailsDialog, setDetailsDialog] = useState(null);
+    const [approveDialog, setApproveDialog] = useState(null)
+    const [rejectDialog, setRejectDialog] = useState(null)
+
+
+    const rejectLeaveApplication = (leaveApplication) => {
+        console.log("Rejected Application of: ", leaveApplication.employee.name);
+    }
 
     const pendignLeaveTableHeader = () => {
         return (
@@ -22,10 +28,10 @@ const PendingLeave = ({ pendingLeaveApplications }) => {
 
     const actionBodyTemplate = (rowData) => {
         return (
-            <div className='flex gap-x-2'>
-                <MdRemoveRedEye onClick={() => setLeaveDetails(rowData)} size={35} color='gray' className='cursor-pointer rounded-full p-2 hover:bg-gray-300' />
-                <AiOutlineCheckCircle size={25} color='green' className='rounded-full cursor-pointer' />
-                <MdOutlineCancel size={25} color='red' className='rounded-full cursor-pointer' />
+            <div className='flex gap-x-2 items-center'>
+                <MdRemoveRedEye onClick={() => setDetailsDialog(rowData)} size={35} color='gray' className='cursor-pointer rounded-full p-2 hover:bg-gray-300' />
+                <AiOutlineCheckCircle onClick={() => setApproveDialog(rowData)} size={25} color='green' className='rounded-full cursor-pointer' />
+                <MdOutlineCancel onClick={() => { setRejectDialog(rowData); rejectLeaveApplication(rowData) }} size={25} color='red' className='rounded-full cursor-pointer' />
             </div>
         )
     }
@@ -45,11 +51,32 @@ const PendingLeave = ({ pendingLeaveApplications }) => {
                 </DataTable>
             </div>
 
+            {/* Application Details Dialog  */}
             <div>
-                <Dialog header="Leave Application Details" visible={leaveDetails} onHide={() => setLeaveDetails(false)}
+                <Dialog header="Leave Application Details" visible={detailsDialog} onHide={() => setDetailsDialog(false)}
                     style={{ width: '50vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
                     <div>
-                        <p>Name: {leaveDetails?.employee?.name}</p>
+                        <p>Name: {detailsDialog?.employee?.name}</p>
+                    </div>
+                </Dialog>
+            </div>
+
+            {/* Approve Application Dialog  */}
+            <div>
+                <Dialog header="Approve Application" visible={approveDialog} onHide={() => setApproveDialog(false)}
+                    style={{ width: '50vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
+                    <div>
+                        <p>Name: {approveDialog?.employee?.name}</p>
+                    </div>
+                </Dialog>
+            </div>
+
+            {/* Delete Application Dialog */}
+            <div>
+                <Dialog header="Confirm Rejection" visible={rejectDialog} onHide={() => setRejectDialog(false)}
+                    style={{ width: '50vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
+                    <div>
+                        <p>Name: {rejectDialog?.employee?.name}</p>
                     </div>
                 </Dialog>
             </div>
