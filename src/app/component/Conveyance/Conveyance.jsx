f5cd0b920f6e1870d117844e7fe8291909a4549d
unknown <ashikahmed121@gmail.com>
2023-08-29 01:07:49 +0600

    sending proper date value from conveyance page to server

--- src/app/component/Conveyance/Conveyance.jsx
+++ src/app/component/Conveyance/Conveyance.jsx
@@ -9,20 +9,23 @@ import { TbReportMoney } from 'react-icons/tb';
 const Conveyance = ({ conveyance, session }) => {
 
     const isFirstRender = useRef(true);
-    const [filterMonth, setFilterMonth] = useState(new Date())
-    const [filterYear, setFilterYear] = useState(new Date())
+    const [selectedMonth, setSelectedMonth] = useState(new Date())
+    const [selectedYear, setSelectedyear] = useState(new Date())
     const [conveyanceData, setConveyanceData] = useState(conveyance)
     const [totalIconColor, setTotalIconColor] = useState('gray')
     const [dueIconColor, setDueIconColor] = useState('gray')
 
     const getConveyanceData = () => {
         console.log("fetching data");
+        const filterMonth = new Date(selectedMonth).getMonth() + 1;
+        const filterYear = new Date(selectedYear).getFullYear();
+        console.log(filterMonth, filterYear);
         const url = `http://localhost:5000/api/v1/conveyance/${session.user.email}?month=${filterMonth}&year=${filterYear}`;
 
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                console.log(data.data);
                 setConveyanceData(data?.data)
             })
     }
@@ -35,13 +38,13 @@ const Conveyance = ({ conveyance, session }) => {
             // Fetch data
             getConveyanceData()
         }
-    }, [filterMonth, filterYear])
+    }, [selectedMonth, selectedYear])
 
     return (
         <div>
             <div className='flex gap-x-2'>
-                <Calendar onChange={(e) => { setFilterMonth(e.value); console.log(e.value); }} value={filterMonth} view="month" dateFormat="MM" size='small' className='p-dropdown-sm' />
-                <Calendar onChange={(e) => { setFilterYear(e.value); console.log(e.value); }} value={filterYear} view="year" dateFormat="yy" size='small' className='p-dropdown-sm' />
+                <Calendar onChange={(e) => { setSelectedMonth((e.value)); console.log(e.value); }} value={selectedMonth} view="month" dateFormat="MM" size='small' className='p-dropdown-sm' />
+                <Calendar onChange={(e) => { setSelectedyear(e.value); console.log(e.value); }} value={selectedYear} view="year" dateFormat="yy" size='small' className='p-dropdown-sm' />
                 {/* <Dropdown options={years} onChange={(e) => { setFilterYear(e.value); }} value={filterYear} size='small' className='p-dropdown-sm' /> */}
             </div>
 
@@ -50,14 +53,14 @@ const Conveyance = ({ conveyance, session }) => {
                     <TbReportMoney size={60} color={totalIconColor} className="" />
                     <div className="flex flex-col justify-center items-center w-[200px] h-[80px] text-center cursor-pointer group-hover:text-white">
                         <p>Total Amount</p>
-                        <p className='text-2xl'>&#2547; 999</p>
+                        <p className='text-2xl'>&#2547; {`${conveyanceData.totalAmount || "00"} `} </p>
                     </div>
                 </div>
                 <div onMouseEnter={() => setDueIconColor('white')} onMouseLeave={() => setDueIconColor('gray')} className="bg-white p-[20px] w-fit rounded-xl shadow-xl flex items-center group hover:bg-violet-400 duration-500">
                     <TbReportMoney size={60} color={dueIconColor} className="" />
                     <div className="flex flex-col justify-center items-center w-[200px] h-[80px] text-center cursor-pointer group-hover:text-white">
                         <p>Due</p>
-                        <p className='text-2xl'>&#2547; 999</p>
+                        <p className='text-2xl'>&#2547; {`${conveyanceData.totalDueAmount || "00"}`}</p>
                     </div>
                 </div>
             </div>
--- src/libs/conveyance.js
+++ src/libs/conveyance.js
@@ -1,5 +1,7 @@
 exports.getConveyanceByEmployeeEmail = async (employeeEmail) => {
-    const conveyance = await fetch(`http://localhost:5000/api/v1/conveyance/${employeeEmail}`, {
+    const month = parseInt(new Date().getMonth() + 1)
+    const year = parseInt(new Date().getFullYear())
+    const conveyance = await fetch(`http://localhost:5000/api/v1/conveyance/${employeeEmail}?month=${month}&year=${year}`, {
         cache: 'no-cache'
     })
         .then(res => res.json())
