5d0cde3d03baeeb0ca94a93adecb2f42c5bafa17
unknown <ashikahmed121@gmail.com>
2023-08-29 20:32:24 +0600

    conveyance details table moved to new component

--- src/app/component/Conveyance/Conveyance.jsx
+++ src/app/component/Conveyance/Conveyance.jsx
@@ -1,24 +1,14 @@
 'use client'
 
-import { Button } from 'primereact/button';
 import { Calendar } from 'primereact/calendar';
-import { Column } from 'primereact/column';
-import { DataTable } from 'primereact/datatable';
-import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
 import React, { useEffect, useRef, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { AiFillPlusSquare } from 'react-icons/ai';
 import { MdOutlinePendingActions } from 'react-icons/md';
 import { TbReportMoney } from 'react-icons/tb';
-import { Toast } from 'primereact/toast';
+import ConveyanceDetailsTable from './ConveyanceDetailsTable';
 
 const Conveyance = ({ conveyance, session }) => {
 
-    const toast = useRef(null)
     const isFirstRender = useRef(true);
-    const { register, control, formState: { errors }, handleSubmit, reset } = useForm();
 
     const [loading, setLoading] = useState(false)
     const [selectedMonth, setSelectedMonth] = useState(new Date())
@@ -26,9 +16,6 @@ const Conveyance = ({ conveyance, session }) => {
     const [conveyanceData, setConveyanceData] = useState(conveyance)
     const [totalIconColor, setTotalIconColor] = useState('gray')
     const [dueIconColor, setDueIconColor] = useState('gray')
-    const [addConveyanceDialog, setAddConveyanceDialog] = useState(false)
-    const [date, setDate] = useState(null);
-    const [attachment, setAttachment] = useState(null);
 
     const getConveyanceData = () => {
         setLoading(true);
@@ -58,60 +45,13 @@ const Conveyance = ({ conveyance, session }) => {
         }
     }, [selectedMonth, selectedYear])
 
-    const handlePhotoChange = (event) => {
-        setAttachment(event.target.files[0]);
-    };
 
-    const handleAddConveyance = (data) => {
-
-        data.date = data.date.toLocaleDateString('en-GB').replace(/\//g, '-').split('-').reverse().join('-');
-        // console.log(data.date);
-        const employee = {
-            name: session.user.name,
-            email: session.user.email
-        }
-        data.employee = employee;
-
-        fetch('http://localhost:5000/api/v1/conveyance', {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.status == "Success") {
-                    console.log("Conveyance created");
-                    getConveyanceData()
-                    toast.current.show({ severity: 'success', summary: 'Success', detail: 'Conveyance Submitted', life: 3000 });
-                }
-                else {
-                    console.log("Failed");
-                    toast.current.show({ severity: 'error', summary: 'Failed!', detail: 'Please try again.', life: 3000 });
-                }
-            })
-
-        setAddConveyanceDialog(false)
-        reset();
-    }
-
-
-    const dateBodyTemplate = (rowData) => {
-        return (
-            <div>
-                {rowData.date.split("T")[0]}
-            </div>
-        )
-    }
 
     return (
         <div>
-            <Toast ref={toast} />
             <div className='flex gap-x-2'>
-                <Calendar onChange={(e) => { setSelectedMonth((e.value)); console.log(e.value); }} value={selectedMonth} view="month" dateFormat="MM" size='small' className='p-dropdown-sm' />
-                <Calendar onChange={(e) => { setSelectedyear(e.value); console.log(e.value); }} value={selectedYear} view="year" dateFormat="yy" size='small' className='p-dropdown-sm' />
+                <Calendar onChange={(e) => { setSelectedMonth((e.value)); console.log(e.value); }} value={selectedMonth} view="month" dateFormat="MM" size='small' />
+                <Calendar onChange={(e) => { setSelectedyear(e.value); console.log(e.value); }} value={selectedYear} view="year" dateFormat="yy" size='small' />
                 {/* <Dropdown options={years} onChange={(e) => { setFilterYear(e.value); }} value={filterYear} size='small' className='p-dropdown-sm' /> */}
             </div>
 
@@ -120,111 +60,22 @@ const Conveyance = ({ conveyance, session }) => {
                     <TbReportMoney size={60} color={totalIconColor} />
                     <div className="flex flex-col justify-center items-center w-[200px] h-[80px] text-center cursor-pointer text-gray-500 group-hover:text-white">
                         <p>Total Amount</p>
-                        <p className='text-3xl font-bold'>&#2547; {`${conveyanceData.totalAmount || "00"} `} </p>
-                        <p className='text-sm mt-2'>Total <span className='text-sky-500 group-hover:text-yellow-300 text-lg font-semibold'>{conveyanceData.totalConveyances}</span> trips found</p>
+                        <p className='text-3xl text-gray-600 group-hover:text-white font-bold'>&#2547; {`${conveyanceData.totalAmount || "00"} `} </p>
+                        <p className='text-xs mt-2'>Found <span className='text-sky-500 group-hover:text-yellow-300 text-[15px] font-semibold'>{conveyanceData.totalConveyances}</span> trips in total</p>
                     </div>
                 </div>
                 <div onMouseEnter={() => setDueIconColor('white')} onMouseLeave={() => setDueIconColor('gray')} className="bg-white p-[20px] w-fit rounded-xl shadow-lg flex items-center group hover:bg-violet-400 duration-500">
                     <MdOutlinePendingActions size={55} color={dueIconColor} />
                     <div className="flex flex-col justify-center items-center w-[200px] h-[80px] text-center cursor-pointer text-gray-500 group-hover:text-white">
                         <p>Due</p>
-                        <p className='text-3xl font-bold'>&#2547; {`${conveyanceData.totalDueAmount || "00"}`}</p>
-                        <p className='text-sm mt-2'>Total <span className='text-sky-500 group-hover:text-yellow-300 text-lg font-semibold'>{conveyanceData.pendingConveyances}</span> trips due</p>
+                        <p className='text-3xl text-gray-600 group-hover:text-white font-bold'>&#2547; {`${conveyanceData.totalDueAmount || "00"}`}</p>
+                        <p className='text-xs mt-2'>Payment due for <span className='text-sky-500 group-hover:text-yellow-300 text-[15px] font-semibold'>{conveyanceData.pendingConveyances}</span> trips</p>
                     </div>
                 </div>
             </div>
 
             <div className='mt-4'>
-                <div className='mt-1 shadow-lg p-2 bg-white rounded-md'>
-                    <div className='flex items-center gap-x-2 mb-2'>
-                        <h3 className='font-light'>CONVEYANCE DETAILS</h3>
-                        <AiFillPlusSquare onClick={() => setAddConveyanceDialog(true)} size={20} color='#8C239E' className='cursor-pointer' />
-                    </div>
-                    {
-                        conveyanceData.conveyanceDetails.length > 0 ?
-                            <DataTable value={conveyanceData.conveyanceDetails} size='small' emptyMessage="No Due Conveyance">
-                                <Column field='from' header="From"></Column>
-                                <Column field='destination' header="Destination"></Column>
-                                <Column body={dateBodyTemplate} header="Date"></Column>
-                                <Column field="amount" header="Amount"></Column>
-                                <Column field="paymentStatus" header="Payment Status"></Column>
-                            </DataTable>
-                            :
-                            <div className='my-4 text-center'>
-                                <p className='bg-sky-400 text-white p-2 inline'>No Due Conveyance</p>
-                            </div>
-                    }
-                </div>
-                {/* add Conveyance dialog  */}
-                <Dialog header="Add Conveyance" visible={addConveyanceDialog} style={{ width: '50vw' }} onHide={() => { setAddConveyanceDialog(false); setDate(null); reset() }}>
-
-                    <form onSubmit={handleSubmit(handleAddConveyance)} className='mt-2'>
-                        <div className='mt-2 flex gap-x-4'>
-                            <div className='w-full'>
-                                {/* <Calendar value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" /> */}
-
-                                {/* <Calendar value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" placeholder='Joining Date' className='w-full' /> */}
-                                <Controller
-                                    name="date"
-                                    control={control}
-                                    rules={{ required: "Date is required" }}
-                                    render={({ field }) => (
-                                        <Calendar
-                                            value={date}
-                                            onChange={(e) => { setDate(e.value); field.onChange(e.value) }}
-                                            placeholder='Date*'
-                                            className='w-full'
-                                            dateFormat="dd-mm-yy"
-                                        />
-                                    )}
-                                />
-                                {errors.date?.type === 'required' && <span className='text-xs text-red-500 block' role="alert">{errors.date.message}</span>}
-                            </div>
-
-                            <div className='w-full'>
-                                <InputText
-                                    {...register("amount", { required: "Amount is required" })}
-                                    keyfilter='int' placeholder="Amount*" className='w-full' />
-                                {errors.amount?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.amount.message}</span>}
-                            </div>
-                        </div>
-                        <div className='mt-2 flex gap-x-4'>
-                            <div className='w-full'>
-                                <InputText
-                                    {...register("from", { required: "From location required" })}
-                                    type='text' placeholder="From*" className='w-full' />
-                                {errors.from?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.from.message}</span>}
-                            </div>
-                            <div className='w-full'>
-                                <InputText
-                                    {...register("destination", { required: "Destination location required" })}
-                                    type='text' placeholder="Destination*" className='w-full' />
-                                {errors.destination?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.destination.message}</span>}
-                            </div>
-                        </div>
-                        <div className='mt-2 flex gap-x-4'>
-                            <div className='w-full'>
-                                <InputText
-                                    {...register("purpose")}
-                                    placeholder="Purpose" className='w-full' />
-                                {errors.purpose?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.purpose.message}</span>}
-                            </div>
-                            <div className='w-full'>
-                                <InputText
-                                    {...register("partner")}
-                                    placeholder="Travel Partner" className='w-full' />
-                                {errors.partner?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.partner.message}</span>}
-                            </div>
-                        </div>
-                        <div className='mt-2'>
-                            <input onChange={handlePhotoChange} name='file' type="file" className='w-full border border-violet-600' />
-                        </div>
-
-                        <div className='mt-4 text-right'>
-                            <Button type='submit' label="Submit" icon="pi pi-check" className="p-button-sm" loading={loading} />
-                        </div>
-                    </form>
-                </Dialog>
+                <ConveyanceDetailsTable conveyanceData={conveyanceData} getConveyanceData={getConveyanceData} session={session} loading={loading} />
             </div>
         </div>
     );
---
+++ src/app/component/Conveyance/ConveyanceDetailsTable.jsx
@@ -0,0 +1,187 @@
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
+import React, { useRef, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { AiFillPlusSquare } from 'react-icons/ai';
+
+const ConveyanceDetailsTable = ({ conveyanceData, getConveyanceData, session, loading }) => {
+
+    const toast = useRef(null)
+    const { register, control, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const [addConveyanceDialog, setAddConveyanceDialog] = useState(false)
+    const [date, setDate] = useState(null);
+    const [attachment, setAttachment] = useState(null);
+
+    const handlePhotoChange = (event) => {
+        setAttachment(event.target.files[0]);
+    };
+
+    const handleAddConveyance = (data) => {
+
+        data.date = data.date.toLocaleDateString('en-GB').replace(/\//g, '-').split('-').reverse().join('-');
+        // console.log(data.date);
+        const employee = {
+            name: session.user.name,
+            email: session.user.email
+        }
+        data.employee = employee;
+
+        fetch('http://localhost:5000/api/v1/conveyance', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.status == "Success") {
+                    console.log("Conveyance created");
+                    getConveyanceData()
+                    toast.current.show({ severity: 'success', summary: 'Success', detail: 'Conveyance Submitted', life: 3000 });
+                }
+                else {
+                    console.log("Failed");
+                    toast.current.show({ severity: 'error', summary: 'Failed!', detail: 'Please try again.', life: 3000 });
+                }
+            })
+
+        setAddConveyanceDialog(false)
+        reset();
+    }
+
+    const buttonTooltipOptions = {
+        position: 'bottom',
+        mouseTrack: true,
+        mouseTrackTop: 25,
+        style: {
+            fontSize: '12px'
+            /* Add any other custom styles here */
+        },
+    };
+
+
+    const dateBodyTemplate = (rowData) => {
+        return (
+            <div>
+                {rowData.date.split("T")[0]}
+            </div>
+        )
+    }
+
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <div className='flex gap-x-2 items-center'>
+                <Button tooltip="View" tooltipOptions={buttonTooltipOptions} icon="pi pi-info" rounded text raised severity='info' aria-label="Filter" />
+                <Button disabled={rowData.paymentStatus !== "Pending"} tooltip="Edit" tooltipOptions={buttonTooltipOptions} icon='pi pi-file-edit' rounded text raised severity='success' />
+                <Button disabled={rowData.paymentStatus !== "Pending"} tooltip="Delete" tooltipOptions={buttonTooltipOptions} icon='pi pi-trash' rounded text raised severity='danger' />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Toast ref={toast} />
+            <div className='mt-1 shadow-lg p-2 bg-white rounded-md'>
+                <div className='flex items-center gap-x-2 mb-2'>
+                    <h3 className='font-light'>CONVEYANCE DETAILS</h3>
+                    <AiFillPlusSquare onClick={() => setAddConveyanceDialog(true)} size={20} color='#8C239E' className='cursor-pointer' />
+                </div>
+                {
+                    conveyanceData.conveyanceDetails.length > 0 ?
+                        <DataTable value={conveyanceData.conveyanceDetails} size='small' emptyMessage="No Due Conveyance">
+                            <Column field='from' header="From"></Column>
+                            <Column field='destination' header="Destination"></Column>
+                            <Column body={dateBodyTemplate} header="Date"></Column>
+                            <Column field="amount" header="Amount"></Column>
+                            <Column field="paymentStatus" header="Payment Status"></Column>
+                            <Column body={actionBodyTemplate} header="Action"></Column>
+                        </DataTable>
+                        :
+                        <div className='my-4 text-center'>
+                            <p className='bg-sky-400 text-white p-2 inline'>No Due Conveyance</p>
+                        </div>
+                }
+            </div>
+            {/* add Conveyance dialog  */}
+            <Dialog header="Add Conveyance" visible={addConveyanceDialog} style={{ width: '50vw' }} onHide={() => { setAddConveyanceDialog(false); setDate(null); reset() }}>
+
+                <form onSubmit={handleSubmit(handleAddConveyance)} className='mt-2'>
+                    <div className='mt-2 flex gap-x-4'>
+                        <div className='w-full'>
+                            {/* <Calendar value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" /> */}
+
+                            {/* <Calendar value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" placeholder='Joining Date' className='w-full' /> */}
+                            <Controller
+                                name="date"
+                                control={control}
+                                rules={{ required: "Date is required" }}
+                                render={({ field }) => (
+                                    <Calendar
+                                        value={date}
+                                        onChange={(e) => { setDate(e.value); field.onChange(e.value) }}
+                                        placeholder='Date*'
+                                        className='w-full'
+                                        dateFormat="dd-mm-yy"
+                                    />
+                                )}
+                            />
+                            {errors.date?.type === 'required' && <span className='text-xs text-red-500 block' role="alert">{errors.date.message}</span>}
+                        </div>
+
+                        <div className='w-full'>
+                            <InputText
+                                {...register("amount", { required: "Amount is required" })}
+                                keyfilter='int' placeholder="Amount*" className='w-full' />
+                            {errors.amount?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.amount.message}</span>}
+                        </div>
+                    </div>
+                    <div className='mt-2 flex gap-x-4'>
+                        <div className='w-full'>
+                            <InputText
+                                {...register("from", { required: "From location required" })}
+                                type='text' placeholder="From*" className='w-full' />
+                            {errors.from?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.from.message}</span>}
+                        </div>
+                        <div className='w-full'>
+                            <InputText
+                                {...register("destination", { required: "Destination location required" })}
+                                type='text' placeholder="Destination*" className='w-full' />
+                            {errors.destination?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.destination.message}</span>}
+                        </div>
+                    </div>
+                    <div className='mt-2 flex gap-x-4'>
+                        <div className='w-full'>
+                            <InputText
+                                {...register("purpose")}
+                                placeholder="Purpose" className='w-full' />
+                            {errors.purpose?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.purpose.message}</span>}
+                        </div>
+                        <div className='w-full'>
+                            <InputText
+                                {...register("partner")}
+                                placeholder="Travel Partner" className='w-full' />
+                            {errors.partner?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.partner.message}</span>}
+                        </div>
+                    </div>
+                    <div className='mt-2'>
+                        <input onChange={handlePhotoChange} name='file' type="file" className='w-full border border-violet-600' />
+                    </div>
+
+                    <div className='mt-4 text-right'>
+                        <Button type='submit' label="Submit" icon="pi pi-check" className="p-button-sm" loading={loading} />
+                    </div>
+                </form>
+            </Dialog>
+        </div>
+    );
+};
+
+export default ConveyanceDetailsTable;
