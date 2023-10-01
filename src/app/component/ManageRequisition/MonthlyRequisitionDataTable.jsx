ef6df57be5088667172014493c1b74760b9c828a
Ashik Ahmed <ashikahmed121@gmail.com>
2023-10-01 10:22:30 +0600

    month filtering and export functionality implemented properly in manage requisition page

--- src/app/component/ManageRequisition/MonthlyRequisitionDataTable.jsx
+++ src/app/component/ManageRequisition/MonthlyRequisitionDataTable.jsx
@@ -1,19 +1,27 @@
 'use client'
+import { getMonthlyRequisitionData } from '@/libs/requisition';
+import { exportRequisition } from '@/utils/exportRequisition';
 import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { Dialog } from 'primereact/dialog';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MdOutlinePendingActions } from 'react-icons/md';
 import { TbReportMoney } from 'react-icons/tb';
 
 const MonthlyRequisitionDataTable = ({ monthlyRequisitionData }) => {
+
+    const isFirstRender = useRef(true);
 
     const [monthlyRequisition, setMonthlyRequisition] = useState(monthlyRequisitionData?.data)
     const [requisitionDetails, setRequisitionDetails] = useState(null)
     const [deleteRequisitionDialog, setDeleteRequisitionDialog] = useState(null)
     const [totalIconColor, setTotalIconColor] = useState('gray')
     const [dueIconColor, setDueIconColor] = useState('gray')
+    const [selectedMonth, setSelectedMonth] = useState(new Date())
+    const [selectedYear, setSelectedyear] = useState(new Date())
+    const [loading, setLoading] = useState(false)
 
     const getRequisitionDetails = (requisitionId) => {
         setLoading(true)
@@ -24,6 +32,25 @@ const MonthlyRequisitionDataTable = ({ monthlyRequisitionData }) => {
                 console.log(data.data);
             })
     }
+
+    useEffect(() => {
+        // Do not fetch data on first render
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+        } else {
+            console.log('fetching data');
+            // Fetch data
+
+            const getRequisitions = async () => {
+                const data = await getMonthlyRequisitionData((selectedMonth.getMonth() + 1), selectedYear.getFullYear())
+                console.log(data);
+
+                setMonthlyRequisition(data.data)
+            }
+
+            getRequisitions()
+        }
+    }, [selectedMonth, selectedYear])
 
 
     const buttonTooltipOptions = {
@@ -57,19 +84,25 @@ const MonthlyRequisitionDataTable = ({ monthlyRequisitionData }) => {
 
     return (
         <div>
+            <div className='flex gap-x-2 mb-2'>
+                <Calendar onChange={(e) => { setSelectedMonth((e.value)); console.log(e.value.getMonth() + 1); }} value={selectedMonth} view="month" yearNavigator={false} style={{ year: { display: "none" } }} className="p-calendar-hide-year"
+                    dateFormat="MM" size='small' />
+                <Calendar onChange={(e) => { setSelectedyear(e.value); console.log(e.value); }} value={selectedYear} view="year" dateFormat="yy" size='small' />
+                {/* <Dropdown options={years} onChange={(e) => { setFilterYear(e.value); }} value={filterYear} size='small' className='p-dropdown-sm' /> */}
+            </div>
             <div className='flex gap-x-2 mb-4'>
                 <div onMouseEnter={() => setTotalIconColor('white')} onMouseLeave={() => setTotalIconColor('gray')} className="bg-white p-[20px] w-fit rounded-xl shadow-lg flex items-center group hover:bg-violet-400 duration-500">
                     <TbReportMoney size={60} color={totalIconColor} />
                     <div className="flex flex-col justify-center items-center w-[200px] h-[80px] text-center cursor-pointer text-gray-500 group-hover:text-white">
                         <p>Total Amount</p>
                         <p className='text-3xl text-gray-600 group-hover:text-white font-bold'>&#2547; {`${monthlyRequisition?.totalProposedAmount || "00"} `} </p>
-                        <p className='text-xs mt-2'>Found <span className='text-sky-500 group-hover:text-yellow-300 text-[15px] font-semibold'>{`${monthlyRequisition?.requisitions || "0"}`}</span> trips in total</p>
+                        <p className='text-xs mt-2'>Found <span className='text-sky-500 group-hover:text-yellow-300 text-[15px] font-semibold'>{`${monthlyRequisition?.requisitions.length || "0"}`}</span> requisitions this month</p>
                     </div>
                 </div>
                 <div onMouseEnter={() => setDueIconColor('white')} onMouseLeave={() => setDueIconColor('gray')} className="bg-white p-[20px] w-fit rounded-xl shadow-lg flex items-center group hover:bg-violet-400 duration-500">
                     <MdOutlinePendingActions size={55} color={dueIconColor} />
                     <div className="flex flex-col justify-center items-center w-[200px] h-[80px] text-center cursor-pointer text-gray-500 group-hover:text-white">
-                        <p>Due Bill</p>
+                        <p>Purchase Amount</p>
                         <p className='text-3xl text-gray-600 group-hover:text-white font-bold'>&#2547; {`${monthlyRequisition?.totalProposedAmount || "00"}`}</p>
                         <p className='text-xs mt-2'>Payment due for <span className='text-sky-500 group-hover:text-yellow-300 text-[15px] font-semibold'>{`${monthlyRequisition?.totalProposedAmount || "0"}`}</span> trips</p>
                     </div>
