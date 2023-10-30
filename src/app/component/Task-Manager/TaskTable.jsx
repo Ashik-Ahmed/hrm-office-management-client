88241adf103000d527e4b12fe032f6156465c91a
Ashik Ahmed <ashikahmed121@gmail.com>
2023-10-30 18:11:31 +0600

    sending query and params in proper way to get all task

--- src/app/component/Task-Manager/TaskTable.jsx
+++ src/app/component/Task-Manager/TaskTable.jsx
@@ -1,25 +1,29 @@
+'use client'
+
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TaskTable = ({ user }) => {
 
     const [tasks, setTasks] = useState();
-    const [query, setQuery] = useState({ currentStatus: 'Open' })
+    const [currentStatus, setCurrentStatus] = useState('Open')
     const [loading, setLoading] = useState(false)
-    const [page, setPage] = useState(1)
+    const [page, setPage] = useState(0)
 
     useEffect(() => {
-        fetch(`http://localhost:5000/api/v1/task/employeeEmail=${user.employeeEmail}?${query}&page=${page}`)
+        const url = `http://localhost:5000/api/v1/task/${user.email}?currentStatus=${currentStatus}&page=${page}`;
+        console.log(url);
+        fetch(url)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
             })
-    }, []);
+    }, [currentStatus, page]);
 
     return (
         <div>
-            <DataTable value={conveyanceData.conveyanceDetails} size='small' emptyMessage="No Due Conveyance" loading={loading}>
+            {/* <DataTable value={conveyanceData.conveyanceDetails} size='small' emptyMessage="No Due Conveyance" loading={loading}>
                 <Column body={dateBodyTemplate} header="Date"></Column>
                 <Column field='from' header="From"></Column>
                 <Column field='destination' header="Destination"></Column>
@@ -27,7 +31,7 @@ const TaskTable = ({ user }) => {
                 <Column field="amount" header="Amount"></Column>
                 <Column field="paymentStatus" header="Payment Status"></Column>
                 <Column body={actionBodyTemplate} header="Action"></Column>
-            </DataTable>
+            </DataTable> */}
         </div>
     );
 };
--- src/app/task-manager/page.jsx
+++ src/app/task-manager/page.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { getAllTasks } from '@/libs/task';
 import { getServerSession } from 'next-auth';
 import TaskTable from '../component/Task-Manager/TaskTable';
 
