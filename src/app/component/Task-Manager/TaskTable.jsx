f4e5ae0d8afd0cd9eee77080827a61b09c7d131c
Ashik Ahmed <ashikahmed121@gmail.com>
2023-10-30 18:33:37 +0600

    tasks loaded successfully and showing in table

--- src/app/component/Task-Manager/TaskTable.jsx
+++ src/app/component/Task-Manager/TaskTable.jsx
@@ -11,27 +11,45 @@ const TaskTable = ({ user }) => {
     const [loading, setLoading] = useState(false)
     const [page, setPage] = useState(0)
 
-    useEffect(() => {
+    const getAllTasks = () => {
+        setLoading(true)
         const url = `http://localhost:5000/api/v1/task/${user.email}?currentStatus=${currentStatus}&page=${page}`;
         console.log(url);
         fetch(url)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
+                if (data.status = "Success") {
+                    setTasks(data.data)
+                }
+                else {
+                    console.log(data.error);
+                }
+                setLoading(false)
             })
-    }, [currentStatus, page]);
+    }
+
+    useEffect(() => {
+        getAllTasks()
+    }, [currentStatus, page, user]);
+
+    const startBodyTemplate = (rowData) => {
+        return (
+            <div>{rowData.createdAt.split("T")[0]}</div>
+        )
+    }
 
     return (
         <div>
-            {/* <DataTable value={conveyanceData.conveyanceDetails} size='small' emptyMessage="No Due Conveyance" loading={loading}>
-                <Column body={dateBodyTemplate} header="Date"></Column>
+            <DataTable value={tasks} size='small' emptyMessage="No tasks found" loading={loading}>
+                <Column body={startBodyTemplate} header="Started"></Column>
                 <Column field='from' header="From"></Column>
                 <Column field='destination' header="Destination"></Column>
                 <Column field='vehicle' header="Vehicle"></Column>
                 <Column field="amount" header="Amount"></Column>
                 <Column field="paymentStatus" header="Payment Status"></Column>
-                <Column body={actionBodyTemplate} header="Action"></Column>
-            </DataTable> */}
+                {/* <Column body={actionBodyTemplate} header="Action"></Column> */}
+            </DataTable>
         </div>
     );
 };
