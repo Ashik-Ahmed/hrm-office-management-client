d97f5515cbf9d0e9892d2a568ff50f99d06416c7
Ashik Ahmed <ashikahmed121@gmail.com>
2023-11-05 20:21:01 +0600

    showing add task dialog

--- src/app/component/Task-Manager/TaskTable.jsx
+++ src/app/component/Task-Manager/TaskTable.jsx
@@ -1,20 +1,32 @@
 'use client'
 
 import Link from 'next/link';
+import { Button } from 'primereact/button';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
+import { Dialog } from 'primereact/dialog';
 import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { AiFillPlusSquare } from 'react-icons/ai';
 import ReactPaginate from 'react-paginate';
 
 const TaskTable = ({ user }) => {
 
+
+    const { register, control, formState: { errors }, handleSubmit, reset } = useForm();
+
     const [tasks, setTasks] = useState();
+    const [addtask, setAddTask] = useState(false)
     const [currentStatus, setCurrentStatus] = useState('Open')
     const [loading, setLoading] = useState(false)
     const [page, setPage] = useState(0);
-    const [pageCount, setPageCount] = useState(2)
+    const [pageCount, setPageCount] = useState(0);
+    const [departments, setDepartments] = useState([]);
+    const [employees, setEmployees] = useState([])
+    const [selectedDepartment, setSelectedDepartment] = useState();
+    const [selectedAssignee, setSelectedAssignee] = useState();
 
     const getAllTasks = () => {
         setLoading(true)
@@ -25,7 +37,8 @@ const TaskTable = ({ user }) => {
             .then(data => {
                 console.log(data);
                 if (data.status = "Success") {
-                    setTasks(data.data)
+                    setTasks(data.data?.tasks)
+                    setPageCount(data.data?.totalPage)
                 }
                 else {
                     console.log(data.error);
@@ -39,7 +52,12 @@ const TaskTable = ({ user }) => {
     }, [currentStatus, page, user]);
 
     const handlePageChange = (page) => {
-        console.log(page.selected + 1);
+        console.log(page.selected);
+        setPage(page.selected);
+    }
+
+    const handleAddNewTask = (data) => {
+        console.log(data);
     }
 
 
@@ -63,7 +81,7 @@ const TaskTable = ({ user }) => {
                 <div className='flex justify-between items-center mb-2'>
                     <div className='flex items-center gap-x-2'>
                         <h3 className='font-light'>TASK LIST</h3>
-                        <AiFillPlusSquare onClick={() => setCreateDepartmentDialog(true)} size={20} color='#8C239E' className='cursor-pointer' />
+                        <AiFillPlusSquare onClick={() => setAddTask(true)} size={20} color='#8C239E' className='cursor-pointer' />
                     </div>
                     <div>
                         <Dropdown options={['Open', 'Closed']} onChange={(e) => { setCurrentStatus(e.value); }} value={currentStatus} size='small' className='p-dropdown-sm' />
@@ -82,7 +100,7 @@ const TaskTable = ({ user }) => {
                         nextLabel=">"
                         onPageChange={handlePageChange}
                         pageRangeDisplayed={10}
-                        pageCount={10}
+                        pageCount={pageCount}
                         previousLabel="<"
                         renderOnZeroPageCount={null}
                         pageClassName='p-1 rounded-sm w-10 hover:bg-gray-200'
@@ -101,6 +119,51 @@ const TaskTable = ({ user }) => {
                     <span className='pi pi-angle-double-right p-2 border-2'></span>
                 </div> */}
             </div>
+
+            <Dialog header="Add Conveyance" visible={addtask} style={{ width: '50vw' }} onHide={() => { setAddTask(false); reset() }}>
+
+                <form onSubmit={handleSubmit(handleAddNewTask)} className='mt-2'>
+                    <div className='mt-2 flex gap-x-4'>
+
+                        <div className='w-full'>
+                            <InputText
+                                {...register("heading", { required: "Heading is required" })}
+                                type='text' placeholder="Heading*" className='w-full' />
+                            {errors.heading?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.heading.message}</span>}
+                        </div>
+                    </div>
+                    <div className='mt-2 flex gap-x-4'>
+                        <div className='w-full'>
+                            <InputText
+                                {...register("description", { required: "Description is required" })}
+                                type='text' placeholder="Description*" className='w-full' />
+                            {errors.description?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.description.message}</span>}
+                        </div>
+                    </div>
+                    <div className='mt-2 flex gap-x-4'>
+                        <div>
+                            <Dropdown
+                                {...register('department', { required: "Department is required" })}
+                                value={selectedDepartment} onChange={(e) => { setSelectedDepartment(e.value) }} options={departments} optionLabel="departmentName" placeholder="Select Department*" className='w-full' />
+                            {errors.department?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.department.message}</span>}
+                        </div>
+                        <div>
+                            <Dropdown
+                                {...register('assignee', { required: "Assignee is required" })}
+                                value={selectedAssignee} onChange={(e) => { setSelectedAssignee(e.value) }} options={employees} optionLabel="name" placeholder="Select Assignee*" className='w-full' />
+                            {errors.assignee?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.assignee.message}</span>}
+                        </div>
+                    </div>
+
+                    {/* <div className='mt-2'>
+        <input onChange={handlePhotoChange} name='file' type="file" className='w-full border border-violet-600' />
+    </div> */}
+
+                    <div className='mt-4 text-right'>
+                        <Button type='submit' label="Submit" className="p-button-sm" loading={loading} />
+                    </div>
+                </form>
+            </Dialog>
         </div>
     );
 };
