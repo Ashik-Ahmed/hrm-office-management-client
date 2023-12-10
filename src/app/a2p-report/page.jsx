a84ffb685df2c0c62f33e9ed476b68579f87c103
Ashik Ahmed <ashikahmed121@gmail.com>
2023-12-10 14:17:32 +0600

    export to excel implemented to a2p report

--- src/app/a2p-report/page.jsx
+++ src/app/a2p-report/page.jsx
@@ -69,8 +69,31 @@ const page = () => {
     }
 
     const exportToExcel = () => {
-        console.log('to excel');
+        import('xlsx').then((xlsx) => {
+            const worksheet = xlsx.utils.json_to_sheet(data);
+            const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+            const excelBuffer = xlsx.write(workbook, {
+                bookType: 'xlsx',
+                type: 'array'
+            });
+
+            saveAsExcelFile(excelBuffer, 'products');
+        });
     }
+
+    const saveAsExcelFile = (buffer, fileName) => {
+        import('file-saver').then((module) => {
+            if (module && module.default) {
+                let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+                let EXCEL_EXTENSION = '.xlsx';
+                const data = new Blob([buffer], {
+                    type: EXCEL_TYPE
+                });
+
+                module.default.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+            }
+        });
+    };
 
     const exportToPDF = () => {
         console.log('to pdf');
