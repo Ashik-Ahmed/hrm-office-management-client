0c261a36af7ddc147145180d6820dbcc2d10e217
Ashik Ahmed <ashikahmed121@gmail.com>
2025-03-21 21:56:07 +0600

    add holiday form created and submit function added

--- src/app/component/Holidays/Holidays.jsx
+++ src/app/component/Holidays/Holidays.jsx
@@ -4,15 +4,23 @@ import { Calendar } from 'primereact/calendar';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { AiFillPlusSquare } from 'react-icons/ai';
 
 const Holidays = ({ holidays }) => {
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
     const [editHolidayDialog, setEditHolidayDialog] = useState(false);
     const [deleteHolidayDialog, setDeleteHolidayDialog] = useState(false);
     const [addHolidayDialog, setAddHolidayDialog] = useState(false);
     const [selectedYear, setSelectedYear] = useState(new Date());
+
+    const handleAddHoliday = (data) => {
+        console.log(data);
+    }
 
     const actionBodyTemplate = (rowData) => {
         return (
@@ -46,7 +54,37 @@ const Holidays = ({ holidays }) => {
             </div>
 
             {/* add holiday dialog  */}
-            <Dialog header="Add Holiday" visible={addHolidayDialog} style={{ width: '50vw' }} onHide={() => { setAddHolidayDialog(false); setDate(null); reset() }}>
+            <Dialog header="Add Holiday" visible={addHolidayDialog} style={{ width: '25vw' }} onHide={() => { setAddHolidayDialog(false); reset() }}>
+
+                <form onSubmit={handleSubmit(handleAddHoliday)} className='flex flex-col gap-y-2'>
+                    <div className='mt-4'>
+                        <InputText
+                            {...register('title', { required: "Title is required" })}
+                            placeholder='Title'
+                            className='w-full'
+                        />
+                        {errors?.title?.type === 'required' && <span className='text-sm text-red-500'>{errors?.title?.message}</span>}
+                    </div>
+                    <div>
+                        <Calendar
+                            {...register('date', { required: "Date is required" })}
+                            placeholder='Date'
+                            className='w-full'
+                        />
+                        {errors?.date?.type === 'required' && <span className='text-sm text-red-500'>{errors?.date?.message}</span>}
+                    </div>
+                    <div>
+                        <InputText
+                            {...register('description')}
+                            placeholder='Description'
+                            className='w-full'
+                        />
+                    </div>
+
+                    <div className='mt-2 flex justify-end'>
+                        <Button type='submit' label='Submit' className='bg-[#8C239E] text-white' />
+                    </div>
+                </form>
 
             </Dialog>
         </div>
