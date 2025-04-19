1db285b2171c0f412bb0828794338c2439e6118c
Ashik Ahmed <ashikahmed121@gmail.com>
2025-04-19 22:46:29 +0600

    header card design completed for attendance report page

--- src/app/attendance-report/page.jsx
+++ src/app/attendance-report/page.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            Manage Attendance
+            <div className='bg-white rounded p-2'>
+                <p className='text-lg font-semibold text-gray-600'>Attendance Details Today</p>
+                <div className='mt-4 border grid grid-cols-3'>
+                    <div className='p-2 border-r'>
+                        <div className='flex items-center gap-2'>
+                            <span className='h-4 w-4 rounded-full inline-block bg-green-600'></span>
+                            <span>Present</span>
+                        </div>
+                        <p className='font-semibold ml-6'>250</p>
+                    </div>
+                    <div className='p-2 border-r'>
+                        <div className='flex items-center gap-2'>
+                            <span className='h-4 w-4 rounded-full inline-block bg-yellow-600'></span>
+                            <span>Late Login</span>
+                        </div>
+                        <p className='font-semibold ml-6'>250</p>
+                    </div>
+                    <div className='p-2 border-r'>
+                        <div className='flex items-center gap-2'>
+                            <span className='h-4 w-4 rounded-full inline-block bg-red-600'></span>
+                            <span>Absent</span>
+                        </div>
+                        <p className='font-semibold ml-6'>250</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
